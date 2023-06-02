c6a4022c1a4fa2de2b6d5708ceae5b73593dbc60
RomanNaumenko <lwoodooz7@gmail.com>
2023-06-02 16:01:36 +0300

    Add user registration view, Change the navbar with appropriate changes and improved visual with bootstrap.

--- club_members/urls.py
+++ club_members/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('login/', views.member_login, name='member-login'),
     path('logout/', views.member_logout, name='member-logout'),
+    path('register/', views.register_user, name='register-user'),
 ]
--- club_members/views.py
+++ club_members/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
 
 
 def member_login(request):
@@ -26,3 +28,20 @@ def member_logout(request):
     messages.success(request, "You were successfully logged out")
     return redirect('home')
 
+
+def register_user(request):
+
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "Registration successful")
+            return redirect('home')
+    else:
+        form = RegisterUserForm()
+
+    return render(request, 'authentication/registration.html', {"form": form})
